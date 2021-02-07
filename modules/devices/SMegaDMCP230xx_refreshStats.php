e4a137a5e2bcf21b54993e5aec68546b42240aa3
Паша <odinwolk@gmail.com>
2021-02-07 17:11:05 +0300

    Create SMegaDMCP230xx_refreshStats.php

---
+++ modules/devices/SMegaDMCP230xx_refreshStats.php
@@ -0,0 +1,10 @@
+<?php
+
+$ot=$this->object_title;
+$ipAddress = $this->getProperty('ipAddress');
+$Password = $this->getProperty('Password');
+$Port = $this->getProperty('PortSDA');
+
+file_get_contents("http://".$ipAddress."/".$Password."/?pt=".$Port."&cnt=0"); //192.168.0.14/sec/?pt=0&cnt=0
+//file_get_contents("http://192.168.10.101/sec/?pt=35&cnt=0");
+//192.168.0.14/sec/?pt=0&cnt=0
