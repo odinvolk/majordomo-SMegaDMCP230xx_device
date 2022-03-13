377e0f9e0ad64d03999efc7b23d2e3312747a6d2
odinvolk <odinwolk@gmail.com>
2022-03-13 07:11:27 +0300

    ooo

--- modules/devices/SMegaDMCP230xx_refreshStats.php
+++ modules/devices/SMegaDMCP230xx_refreshStats.php
@@ -5,6 +5,4 @@ $ipAddress = $this->getProperty('ipAddress');
 $Password = $this->getProperty('Password');
 $Port = $this->getProperty('PortSDA');
 
-file_get_contents("http://".$ipAddress."/".$Password."/?pt=".$Port."&cnt=0"); //192.168.0.14/sec/?pt=0&cnt=0
-//file_get_contents("http://192.168.10.101/sec/?pt=35&cnt=0");
-//192.168.0.14/sec/?pt=0&cnt=0
+file_get_contents("http://".$ipAddress."/".$Password."/?pt=".$Port."&cnt=0"); //192.168.0.14/sec/?pt=35&cnt=0
